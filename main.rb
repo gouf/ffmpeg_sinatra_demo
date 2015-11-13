b18cd84b72a961e42448ebd67b78f1dd57d73152
Gou Furuya <innocent.zero@gmail.com>
2015-11-13 13:33:40 +0900

    Cleanup commented out code

--- main.rb
+++ main.rb
@@ -1,15 +1,6 @@
-# 読み込み部分
 require 'streamio-ffmpeg'
 require 'fileutils'
 require 'tempfile'
-
-# 処理部分
-# version :screenshot do
-#   process :screenshot
-#   def full_filename(_for_file = model.logo.file)
-#     'screenshot.jpg'
-#   end
-# end
 
 def screenshot
   current_path = '/home/gou/ffmpeg/movie.mp4'
@@ -19,15 +10,9 @@ def screenshot
 
   movie = FFMPEG::Movie.new(current_path)
   movie.screenshot(tempfile.path, resolution: '512x312', preserve_aspect_ratio: :width)
-  # File.open(File.expand_path('./thumbnail.jpg'), 'w') do |f|
-  #   f.write tempfile.read
-  # end
+
   FileUtils.cp(tempfile.path, File.expand_path('./public'))
   tempfile.unlink
-
-  # File.rename("#{current_path}.jpg", current_path)
-
-  # File.delete(tmpfile)
 end
 
 def images
